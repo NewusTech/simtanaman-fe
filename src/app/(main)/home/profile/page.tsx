7a52d2abc3cfc27025d2935460c6eabe312ab9f0
dede sudrajat <sudrajatdede23@gmail.com>
2025-05-04 10:23:34 +0700

    feat(profile-page): add tab navigation for personal information and work area

--- src/app/(main)/home/profile/page.tsx
+++ src/app/(main)/home/profile/page.tsx
@@ -14,6 +14,7 @@ export default function ProfilePage() {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1); // Define currentPage with an initial value
+  const [currentTab, setCurrentTab] = useState(0);
   const [formData, setFormData] = useState({
     id: 0,
     email: "",
@@ -83,20 +84,58 @@ export default function ProfilePage() {
           </Button>
         </div>
       </div>
-      <div className="flex flex-col md:flex-row justify-between items-start mt-4 w-full gap-4 mb-4">
-        <div className="flex flex-col items-center w-full gap-4">
-          <FormLabel label="Nama" value={formData.name} />
-          <FormLabel label="Tanggal Lahir" value={formData.tglLahir} />
-          <FormLabel label="NIP" value={String(formData.nip)} />
-          <FormLabel label="Nomor Telepon" value={String(formData.noHp)} />
-        </div>
-        <div className="flex flex-col items-center w-full gap-4">
-          <FormLabel label="NIK" value={String(formData.nik)} />
-          <FormLabel label="Jenis Kelamin" value={String(formData.jenisKelamin)} />
-          <FormLabel label="Email" value={formData.email} />
-          <FormLabel label="Alamat" value={formData.alamat} />
-        </div>
+      <div className="flex w-[20rem]">
+        <button
+          onClick={() => setCurrentTab(0)}
+          className={`flex-1 py-2 rounded-l-full ${
+            currentTab === 0
+              ? "bg-primary-default text-white"
+              : "bg-gray-100 text-gray-700"
+          }`}
+        >
+          Informasi
+        </button>
+        <button
+          onClick={() => setCurrentTab(1)}
+          className={`flex-1 py-2 rounded-r-full ${
+            currentTab === 1
+              ? "bg-primary-default text-white"
+              : "bg-gray-100 text-gray-700"
+          }`}
+        >
+          Wilayah Kerja
+        </button>
       </div>
+      {(currentTab === 0 && (
+        <div className="flex flex-col md:flex-row justify-between items-start mt-4 w-full gap-4 mb-4">
+          <div className="flex flex-col items-center w-full gap-4">
+            <FormLabel label="Nama" value={formData.name} />
+            <FormLabel label="Tanggal Lahir" value={formData.tglLahir} />
+            <FormLabel label="NIP" value={String(formData.nip)} />
+            <FormLabel label="Nomor Telepon" value={String(formData.noHp)} />
+          </div>
+          <div className="flex flex-col items-center w-full gap-4">
+            <FormLabel label="NIK" value={String(formData.nik)} />
+            <FormLabel
+              label="Jenis Kelamin"
+              value={String(formData.jenisKelamin)}
+            />
+            <FormLabel label="Email" value={formData.email} />
+            <FormLabel label="Alamat" value={formData.alamat} />
+          </div>
+        </div>
+      )) || (
+        <div className="flex flex-col md:flex-row justify-between items-start mt-4 w-full gap-4 mb-4">
+          <div className="flex flex-col items-center w-full gap-4">
+            <FormLabel label="Provinsi" value="Placeholder" />
+            <FormLabel label="Kecamatan" value="Placeholder" />
+          </div>
+          <div className="flex flex-col items-center w-full gap-4">
+            <FormLabel label="Kabupaten/Kota" value="Placeholder" />
+            <FormLabel label="Desa/Binaan" value="Placeholder" />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
